411350f98a23d66cfc9677ed0e322d41d7031022
Estevao Souza <estevao_souza16@hotmail.com>
2022-10-22 20:19:43 -0300

    Criação do projeto

---
+++ src/main/java/com/ecangussu/springmongo/ApiRestfulSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.ecangussu.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecangussu/springmongo/ApiRestfulSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecangussu.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
