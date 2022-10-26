9329417ab02155e6e36ede91168ce3592278f07a
Estevao Souza <estevaosouza18@gmail.com>
2022-10-25 21:28:11 -0300

    Projeção dos dados do autor de um post com DTO

--- src/main/java/com/ecangussu/springmongo/config/Instantiation.java
+++ src/main/java/com/ecangussu/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ecangussu.springmongo.domain.Post;
 import com.ecangussu.springmongo.domain.User;
+import com.ecangussu.springmongo.dto.AuthorDTO;
 import com.ecangussu.springmongo.repositories.PostRepository;
 import com.ecangussu.springmongo.repositories.UserRepository;
 
@@ -33,16 +34,16 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		userRepository.saveAll(Arrays.asList(estevao, alex, bob));
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		// Ao colocarmos o User no fina, estamos passando uma cópia e não uma referência
-		// do objeto = coceito de objeto aninhado
 		Post post1 = new Post(null, sdf.parse("25/10/2022"), "Jogo do título!",
-				"Palmeiras pode ganhar seu 11º título brasileiro hoje!", estevao);
-		Post post2 = new Post(null, sdf.parse("17/11/2013"), "20 anos!", "Hoje completo 20 anos!", estevao);
+				"Palmeiras pode ganhar seu 11º título brasileiro hoje!", new AuthorDTO(estevao));
+		Post post2 = new Post(null, sdf.parse("17/11/2013"), "20 anos!", "Hoje completo 20 anos!",
+				new AuthorDTO(estevao));
 
-		userRepository.saveAll(Arrays.asList(estevao, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
