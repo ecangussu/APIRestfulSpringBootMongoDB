0d57f823e4d955bad363be3de6020bece894d3e4
Estevao Souza <estevaosouza18@gmail.com>
2022-10-26 21:54:10 -0300

    Criação de endpoint que retorna os posts dos usuarios

--- src/main/java/com/ecangussu/springmongo/resources/UserResource.java
+++ src/main/java/com/ecangussu/springmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ecangussu.springmongo.domain.Post;
 import com.ecangussu.springmongo.domain.User;
 import com.ecangussu.springmongo.dto.UserDTO;
 import com.ecangussu.springmongo.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		User user = userService.fromDTO(userDTO);
 		userService.update(id, user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
